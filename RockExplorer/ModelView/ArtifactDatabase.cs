68b8afa4987bc1c061c7b03548479c96907796be
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-05-08 18:50:36 +0200

    Inherited ICRUD interface.

--- RockExplorer/ModelView/ArtifactDatabase.cs
+++ RockExplorer/ModelView/ArtifactDatabase.cs
@@ -1,6 +1,40 @@
-﻿namespace RockExplorer.ModelView
+﻿/*
+ * Author: Gabriel H. Kierkegaard
+ * Co-author: 
+ * Date: 08-05-2023
+ */
+
+using RockExplorer.Model;
+
+namespace RockExplorer.ModelView
 {
-    public class ArtifactDatabase
+    public class ArtifactDatabase : ICRUD<Artifact>
     {
+        public ArtifactDatabase() { }
+
+        public Artifact Create(Artifact entity)
+        {
+            throw new NotImplementedException();
+        }
+        public Artifact Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Artifact> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+        public void Update(Artifact entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
