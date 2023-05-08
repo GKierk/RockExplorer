7f9abc62aba193c34bf64995e9c48e0549e8a439
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-05-08 18:47:18 +0200

    Implemented ICrud interface.

--- RockExplorer/Model/ICrud.cs
+++ RockExplorer/Model/ICrud.cs
@@ -1,6 +1,16 @@
-﻿namespace RockExplorer.Model
+﻿/*
+ * Author: Gabriel H. Kierkegaard
+ * Date: 08-05-2023
+ */
+
+namespace RockExplorer.Model
 {
-    public class ICrud
+    public interface ICrud<T>
     {
+        public T Create(T entity);
+        public T Read(int id);
+        public List<T> ReadAll();
+        public void Update(T entity);
+        public void Delete(int id);
     }
 }
