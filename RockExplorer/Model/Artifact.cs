5ffe839c39fa419f3f21725e3c05f3dc49a084fe
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-05-09 18:46:26 +0200

    Commit for Gül and Nur - Implemented Artifact class.

--- RockExplorer/Model/Artifact.cs
+++ RockExplorer/Model/Artifact.cs
@@ -1,6 +1,20 @@
-﻿namespace RockExplorer.Model
+﻿/*
+ *Authors: Gülsüm og Nuriye Erdogan
+ *Date: 09-05-2023
+ */
+namespace RockExplorer.Model
 {
     public class Artifact
     {
+
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? PathToAudioFile { get; set; }
+        public string? PathToImage { get; set; }
+        public int YearOfCreation { get; set; }
+        public string? Artist { get; set; }
+
     }
+
 }
