5b881b1c8f50aa9b00f3013a15675e0f7964e2fd
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-05-09 15:12:32 +0200

    Implemented Person class.

--- RockExplorer/Model/Person.cs
+++ RockExplorer/Model/Person.cs
@@ -1,6 +1,17 @@
-﻿namespace RockExplorer.Model
+﻿/*
+ * Author: Gabriel H. Kierkegaard
+ * Date: 2023-05-09
+ */
+
+namespace RockExplorer.Model
 {
     public class Person
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public int MobileNumber { get; set; }
+        public string? Email { get; set; }
+        public bool IsEmployee { get; set; }
     }
 }
