f79ac317fe378529e8e05d808630c00dcb2f84d5
Mohamad Kassem <moha226i@edu.zealand.dk>
2023-05-10 10:15:07 +0200

    Commented out

--- RockExplorer/ModelView/ArtifactDatabase.cs
+++ RockExplorer/ModelView/ArtifactDatabase.cs
@@ -4,37 +4,37 @@
  * Date: 08-05-2023
  */
 
-using RockExplorer.Model;
+//using RockExplorer.Model;
 
-namespace RockExplorer.ModelView
-{
-    public class ArtifactDatabase : ICRUD<Artifact>
-    {
-        public ArtifactDatabase() { }
+//namespace RockExplorer.ModelView
+//{
+//    public class ArtifactDatabase : ICRUD<Artifact>
+//    {
+//        public ArtifactDatabase() { }
 
-        public Artifact Create(Artifact entity)
-        {
-            throw new NotImplementedException();
-        }
-        public Artifact Read(int id)
-        {
-            throw new NotImplementedException();
-        }
+//        public Artifact Create(Artifact entity)
+//        {
+//            throw new NotImplementedException();
+//        }
+//        public Artifact Read(int id)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public List<Artifact> ReadAll()
-        {
-            throw new NotImplementedException();
-        }
-        public void Update(Artifact entity)
-        {
-            throw new NotImplementedException();
-        }
+//        public List<Artifact> ReadAll()
+//        {
+//            throw new NotImplementedException();
+//        }
+//        public void Update(Artifact entity)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public void Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
+//        public void Delete(int id)
+//        {
+//            throw new NotImplementedException();
+//        }
 
 
-    }
-}
+//    }
+//}
