64a2cf47de526ded793a3fdfd376c5dc4a12112a
Mohamad Kassem <moha226i@edu.zealand.dk>
2023-05-10 10:13:15 +0200

    Created a ICRUD, that defines methodes of CRUD

---
+++ RockExplorer/Interfaces/ICRUD.cs
@@ -0,0 +1,14 @@
+﻿using RockExplorer.Model;
+// Denne metode beskriver et interface kaldet ICRUD, som definerer en række metoder til at udføre CRUD-operationer (Create, Read, Update og Delete) på en samling af "Artifact"-objekter.
+namespace RockExplorer.Interfaces
+{
+    public interface ICRUD
+    {
+        Dictionary<int, Artifact> AllArtifacts();
+        Dictionary<int, Artifact> FilterArtifact(string crtiteria);
+        void Delete(int id);
+        void AddArtifact(Artifact Artifact);
+        void UpdateArtifact(Artifact Artifact);
+        Artifact GetArtifact(int id);
+    }
+}
