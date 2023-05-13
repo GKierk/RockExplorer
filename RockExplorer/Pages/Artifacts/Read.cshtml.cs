8d67c323fb3cccca1a87ceefca400e44377372d5
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-05-13 16:38:03 +0200

    Commented out some errors.

--- RockExplorer/Pages/Artifacts/Read.cshtml.cs
+++ RockExplorer/Pages/Artifacts/Read.cshtml.cs
@@ -1,6 +1,7 @@
 /*
  *Authors: Mohamad Kassem
  *Date: 09-05-2023
+ *Edited by: Gabriel H. Kierkegaard, Date: 13-05-2023
  */
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,26 +12,27 @@ using RockExplorer.Model;
 namespace RockExplorer.Pages.Artifacts__CRUD_
 { 
    public class Read : PageModel
-{
-    private ICRUD catalog;
-        public Read(ICRUD repository)
-    {
-        catalog = repository;
-    }
-    public Dictionary<int, Artifact> Artifacts { get; private set; }
-
-    [BindProperty(SupportsGet = true)]
-    public string FilterCriteria { get; set; }
-
-    public IActionResult OnGet()
-    {
-        Artifacts = catalog.AllArtifacts();
-        if (!string.IsNullOrEmpty(FilterCriteria))
+   {
+        //private ICRUD catalog;
+        private ArtifactCatalog catalog;
+        public Read()
         {
-            Artifacts = catalog.FilterArtifact(FilterCriteria);
+            catalog = ArtifactCatalog.GetInstance();
         }
+        public Dictionary<int, Artifact> Artifacts { get; private set; }
 
-        return Page();
-    }
-}
+        [BindProperty(SupportsGet = true)]
+        public string FilterCriteria { get; set; }
+
+        public IActionResult OnGet()
+        {
+            Artifacts = catalog.ReadAll();
+            if (!string.IsNullOrEmpty(FilterCriteria))
+            {
+                Artifacts = catalog.FilterArtifact(FilterCriteria);
+            }
+
+            return Page();
+        }
+   }
 }
