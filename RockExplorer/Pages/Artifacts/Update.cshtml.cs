afe15cf6aa903d70d2c61d81298bcac00110f42f
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-05-13 16:26:18 +0200

    Updated Project Structure.

---
+++ RockExplorer/Pages/Artifacts/Update.cshtml.cs
@@ -0,0 +1,37 @@
+/*
+ *Authors: Mohamad Kassem
+ *Date: 09-05-2023
+ */
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RockExplorer.Interfaces;
+using RockExplorer.Model;
+
+// denne kode gør det muligt for brugeren at opdatere en eksisterende "Artifact" i databasen ved at udfylde en formular og klikke på en knap.
+namespace RockExplorer.Pages.Artifacts__CRUD_
+{
+    public class Update : PageModel
+    {
+        [BindProperty]
+        public Artifact artifact { get; set; }
+        private ICRUD catalog;
+        public Update(ICRUD repository)
+        {
+            catalog = repository;
+        }
+        public void OnGet(int id)
+        {
+            artifact = catalog.GetArtifact(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            catalog.UpdateArtifact(artifact);
+            return RedirectToPage("GetAllPizzas");
+        }
+    }
+}
