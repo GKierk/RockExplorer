5b05bc45e17dc3b20eddd1ccf3ed4cdde27dccd4
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-05-13 16:30:17 +0200

    Added to source control.

---
+++ RockExplorer/Pages/Artifacts/Artifacts.cshtml.cs
@@ -0,0 +1,27 @@
+/*
+ * Author: Gabriel H. Kierkegaard, Date: 13-05-2023
+ */
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RockExplorer.Model;
+
+namespace RockExplorer.Pages.Artifacts
+{
+    public class ArtifactsModel : PageModel
+    {
+        private ArtifactCatalog catalog;
+
+        public Dictionary<int, Artifact> Artifacts { get; private set; }
+
+        public ArtifactsModel()
+        {
+            catalog = new ArtifactCatalog();
+        }
+
+        public void OnGet()
+        {
+            Artifacts = catalog.ReadAll();
+        }
+    }
+}
