e48a82ea639dd6f91e1fb443ee9cb0face3bf0b5
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-05-13 16:05:51 +0200

    Removed ID.

--- RockExplorer/Model/Artifact.cs
+++ RockExplorer/Model/Artifact.cs
@@ -6,15 +6,12 @@ namespace RockExplorer.Model
 {
     public class Artifact
     {
-
-        public int ID { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string? PathToAudioFile { get; set; }
         public string? PathToImage { get; set; }
         public int YearOfCreation { get; set; }
         public string? Artist { get; set; }
-
     }
 
 
