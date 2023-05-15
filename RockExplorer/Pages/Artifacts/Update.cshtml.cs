f1616d75aa5d46b1b3c7f4e4cbe6e0728a02841c
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-05-15 13:32:03 +0200

    Called singleton property instead of method.

--- RockExplorer/Pages/Artifacts/Update.cshtml.cs
+++ RockExplorer/Pages/Artifacts/Update.cshtml.cs
@@ -2,6 +2,7 @@
  *Authors: Mohamad Kassem
  *Date: 09-05-2023
  *Edited by: Gabriel H. Kierkegaard, Date: 14-05-2023
+ *Edited by: Gabriel H. Kierkegaard, Date: 15-05-2023
  */
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -20,7 +21,7 @@ namespace RockExplorer.Pages.Artifacts__CRUD_
         //private ICRUD catalog;
         public Update()
         {
-            catalog = ArtifactCatalog.GetInstance();
+            catalog = ArtifactCatalog.Instace;
         }
         public void OnGet(int id)
         {
