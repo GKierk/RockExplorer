72d0210158df0e5ceb458edf51cc22d4cb5f6541
Gabriel H. Kierkegaard <gabriel_kierkegaard@protonmail.com>
2023-05-20 18:41:59 +0200

    Implemented Jason File Handler to read and write to Json file format.

---
+++ RockExplorer/Helpers/JsonFileHandler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using RockExplorer.Model;
+using System.Text.Json.Serialization;
+using RockExplorer.ModelView;
+
+namespace RockExplorer.Helpers
+{
+    public class JsonFileHandler
+    {
+        public static Dictionary<int, Artifact> ReadJson(string JsonFileName)
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                if (JsonFileName != null)
+                {
+                    return JsonSerializer.Deserialize<Dictionary<int, Artifact>>(jsonFileReader.ReadToEnd());
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static void WriteToJson(string JsonFileName)
+        {
+            ArtifactCatalog catalog = ArtifactCatalog.Instance;
+
+            using (FileStream outputStream = File.OpenWrite(JsonFileName))
+            {
+                var writer = new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                {
+                    SkipValidation = false,
+                    Indented = true
+                });
+
+                JsonSerializer.Serialize<Dictionary<int, Artifact>>(writer, catalog.Artifacts);
+            }
+        }
+    }
+}
